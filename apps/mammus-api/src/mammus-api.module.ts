a1e907e4dd44ead3ebab3734f1542d86e2a0dfe0
Casca0 <lucashborzani@gmail.com>
2025-04-17 23:46:23 -0300

    fix: added import for httpmodule

--- apps/mammus-api/src/mammus-api.module.ts
+++ apps/mammus-api/src/mammus-api.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { MammusApiController } from './mammus-api.controller';
 import { MammusApiService } from './mammus-api.service';
 import { ChatGatewayModule } from './chat-gateway/chat-gateway.module';
 
 @Module({
-	imports: [ChatGatewayModule],
+	imports: [ChatGatewayModule, HttpModule],
 	controllers: [MammusApiController],
 	providers: [MammusApiService],
 })
